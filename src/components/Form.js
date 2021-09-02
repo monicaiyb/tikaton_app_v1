102728d55f76f60600d07e6e552c035eb6cbc430
monicaiyb <monicaiybgmail.comgit config --global user.name monicaiybgit config --global user.email monicaiybgmail.com>
2021-09-02 20:32:12 +0300

    Update submit on signup.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,117 +1,127 @@
-import React from 'react'
-import { Container, Col, Row, Button, Form, Card } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Container, Col, Row, Button, Form, Card } from "react-bootstrap";
 
-// const handleSubmit = event => {
-//     event.preventDefault();
-//     alert("you have submited form")
-// }
+const SignUpForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [phone, setPhone] = useState("");
+  const [department, setDepartment] = useState("");
+  const [password, setPassword] = useState("");
 
-const SignUpForm = (event) => {
-    event.preventDefault();
-    const submitUserData = async () => {
-    const incomeEndPoint = 'http://10.0.2.2:3500/amtIncome/addIncome';
-        const data = [{
-          firstName:"",
-          lastName:"",
-          phone:"",
-          password: "",
-          username:"",
-        }]
-          fetch(incomeEndPoint, {
-            method: 'post',
-            headers:{
-                'Content-Type': 'application/json',
-               
-            },
+  const handleSubmit = (event) => {
+    // event.preventDefault();
+    // const {firstName, username, lastName, phone, password } = event.target.elements
+    console.log(firstName);
 
-            body: JSON.stringify(data),
-          })
-          .then(res=>{
-           
-            return res.json()
-          })
-            // eslint-disable-next-line no-shadow
-            .then(data => {
-              console.log(data)
-            })  
-            
+    // const data=[]
+    //     const userEndPoint = 'http://10.0.2.2:3500/tikaton/users';
+    //     fetch(userEndPoint, {
+    //         method: 'post',
+    //         headers:{
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(data),
+    //       })
+    //       .then(res=>{
+    //         return res.json()
+    //       })
+    // eslint-disable-next-line no-shadow
+    // .then(data => {
+    //   console.log(data)
+    // })
+  };
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+        <Col xs={6}>
+          <Card className="mx-auto my-2">
+            <Card.Body>
+              <Card.Title>
+                <div className="mb-6">
+                  <h3>SignUp Form</h3>
+                </div>
+              </Card.Title>
+              <div className="mb-6">
+                <Form onSubmit={handleSubmit}>
+                  <div>
+                    <Form.Text className="text-muted">
+                      Please sign in here
+                    </Form.Text>
+                    <Form.Group className="mb-3 mt-6">
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter the first name"
+                        name="firstName"
+                        value={firstName}
+                        onChange={(event) => setFirstName(event.target.value)}
+                      />
+                    </Form.Group>
+                    <Form.Group className="mb-3 mt-6">
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter the Last name"
+                        name="lastName"
+                        value={lastName}
+                        onChange={(event) => setlastName(event.target.value)}
+                       
+                      />
+                    </Form.Group>
+                    <Form.Group className="mb-3 mt-6">
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter a username"
+                        name="username"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                      />
+                    </Form.Group>
+                    <Form.Group className="mb-3 mt-6">
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter your phone"
+                        name="phone"
+                        value={phone}
+                        onChange={(event) => setPhone(event.target.value)}
+                      />
+                    </Form.Group>
+                    <Form.Group className="mb-3 mt-6">
+                      <Form.Select name="department" value={department}
+                        onChange={(event) => setDepartment(event.target.value)}
+                        >
+                        <option>Department</option>
+                        <option>Production</option>
+                        <option>Sales</option>
+                        <option>Administrator</option>
+                        <option></option>
+                      </Form.Select>
+                    </Form.Group>
 
-      };
-  
-    
+                    <Form.Group className="mb-3" >
+                      <Form.Control
+                        type="password"
+                        placeholder="Enter the Password"
+                        name="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                      />
+                    </Form.Group>
+                  </div>
+                  <div className="d-grid gap-2">
+                    <Button type="submit" variant="primary" name="submit">
+                      Submit
+                    </Button>
+                  </div>
+                </Form>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+};
 
-    return (
-        <Container>
-            <Row>
-
-                <Col></Col>
-                <Col xs={6}>
-                    <Card className="mx-auto my-2">
-                        <Card.Body>
-                            <Card.Title><div className="mb-6">
-                                <h3>SignUp Form</h3> 
-                                </div></Card.Title>
-                            <div className="mb-6">
-
-                                <Form onSubmit={submitUserData}>
-                                    <div>
-                                        <Form.Text className="text-muted">
-                                            Please sign in here
-                                        </Form.Text>
-                                        <Form.Group className="mb-3 mt-6">
-
-                                            <Form.Control type="text" placeholder="Enter the first name" name="firstName" />
-
-                                        </Form.Group>
-                                        <Form.Group className="mb-3 mt-6">
-
-                                            <Form.Control type="text" placeholder="Enter the Last name" name="lastName" />
-
-                                        </Form.Group>
-                                        <Form.Group className="mb-3 mt-6">
-
-                                            <Form.Control type="text" placeholder="Enter a username" name="username" />
-
-                                        </Form.Group>
-                                        <Form.Group className="mb-3 mt-6">
-
-                                            <Form.Control type="text" placeholder="Enter your phone" name="phone" />
-
-                                        </Form.Group>
-                                        <Form.Group className="mb-3 mt-6" >
-                                            
-                                            <Form.Select name="department">
-                                                <option>Department</option>
-                                                <option>Production</option>
-                                                <option>Sales</option>
-                                                <option>Administrator</option>
-                                                <option></option>
-                                            </Form.Select>                                           
-                                        </Form.Group>
-
-
-                                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                                            <Form.Control type="password" placeholder="Enter the Password" name="password" />
-                                        </Form.Group>
-
-                                    </div>
-                                    <div className="d-grid gap-2">
-                                        <Button type="submit" variant="primary" name="submit">Submit</Button>
-                                    </div>
-                                </Form>
-                            </div>
-                        </Card.Body>
-                    </Card>
-
-
-                </Col>
-                <Col></Col>
-
-            </Row>
-        </Container>
-
-
-    )
-}
-
-export default SignUpForm
+export default SignUpForm;
